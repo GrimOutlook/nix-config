dd439074b59774bc3b488fc42d1919933c02a51a
Dominic Grimaldi <self@grimoutlook.dev>
2026-01-28 15:05:01 -0600

    Add initial NixOS flake configuration

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+        nixos-wsl.nixosModules.default
+
+        {
+          system.stateVersion = "25.05";
+          wsl.enable = true;
+        }
+      ];
+    };
+  };
+}

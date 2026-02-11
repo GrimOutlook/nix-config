424a211441ad229aea49ff52f3fd9b55d566945a
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-11 08:43:05 -0600

    wip!

--- modules/home/core/programs/shell/bat.nix
+++ modules/home/core/programs/shell/bat.nix
@@ -6,7 +6,8 @@
     };
 
     home.sessionVariables = {
-      MANPAGER = "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'";
+      # TODO: Figure out why this isn't working
+      # MANPAGER = "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'";
     };
 
     home.shellAliases = {
--- modules/hosts/horizon/_nixos/default.nix
+++ modules/hosts/horizon/_nixos/default.nix
@@ -1,7 +1,5 @@
 {config, inputs, lib, self, ...}:
 {
-  age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKioB5C0OpoFNFHKEaKFGcaI50070Q/NvYUM0SMgxVP";
-  
   system = {
     autoUpgrade.enable = false;
     stateVersion = "25.05";

f149cd786084663926a475cba4837ddc9868d951
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-04 04:08:35 -0600

    Allow root login with keys

--- modules/core/ssh-server.nix
+++ modules/core/ssh-server.nix
@@ -4,7 +4,7 @@
       enable = true;
 
       settings = {
-        PermitRootLogin = "no";
+        PermitRootLogin = "prohibit-password";
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
       };

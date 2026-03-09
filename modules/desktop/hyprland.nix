9dbfb9209a284f4350de7a92b09bafc12303ed8a
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-08 20:33:48 -0500

    feat: Add hyprland config

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -14,9 +14,142 @@
             hy3
           ];
           settings = {
+
+            ################
+            ### PROGRAMS ###
+            ################
             "$terminal" = "alacritty";
             "$menu" = "rofi -show run";
 
+            #####################
+            ### LOOK AND FEEL ###
+            #####################
+
+            # Refer to https://wiki.hyprland.org/Configuring/Variables/
+
+            # https://wiki.hyprland.org/Configuring/Variables/#general
+            general = {
+              gaps_in = 5;
+              gaps_out = 20;
+
+              border_size = 2;
+
+              # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+              "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+              "col.inactive_border" = "rgba(595959aa)";
+
+              # Set to true enable resizing windows by clicking and dragging on borders and gaps
+              resize_on_border = true;
+              hover_icon_on_border = true;
+
+              # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+              allow_tearing = false;
+
+              layout = "hy3";
+            };
+
+            # https://wiki.hyprland.org/Configuring/Variables/#decoration
+            decoration = {
+              rounding = 10;
+              rounding_power = 2;
+
+              # # Change transparency of focused and unfocused windows
+              # active_opacity = 1.0
+              # inactive_opacity = 0.90
+
+              shadow = {
+                enabled = true;
+                range = 4;
+                render_power = 3;
+                color = "rgba(1a1a1aee)";
+              };
+
+              # https://wiki.hyprland.org/Configuring/Variables/#blur
+              blur = {
+                enabled = true;
+                size = 3;
+                passes = 1;
+
+                vibrancy = 0.1696;
+              };
+            };
+
+            # https://wiki.hyprland.org/Configuring/Variables/#animations
+            animations = {
+              enabled = "yes"; # , please :)
+              workspace_wraparound = true;
+
+              # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+              bezier = [
+                "easeOutQuint,0.23,1,0.32,1"
+                "easeInOutCubic,0.65,0.05,0.36,1"
+                "linear,0,0,1,1"
+                "almostLinear,0.5,0.5,0.75,1.0"
+                "quick,0.15,0,0.1,1"
+              ];
+
+              animation = [
+                "global, 1, 10, default"
+                "border, 1, 5.39, easeOutQuint"
+                "windows, 1, 4.79, easeOutQuint"
+                "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+                "windowsOut, 1, 1.49, linear, popin 87%"
+                "fadeIn, 1, 1.73, almostLinear"
+                "fadeOut, 1, 1.46, almostLinear"
+                "fade, 1, 3.03, quick"
+                "layers, 1, 3.81, easeOutQuint"
+                "layersIn, 1, 4, easeOutQuint, fade"
+                "layersOut, 1, 1.5, linear, fade"
+                "fadeLayersIn, 1, 1.79, almostLinear"
+                "fadeLayersOut, 1, 1.39, almostLinear"
+                "workspaces, 1, 1.94, almostLinear, fade"
+                "workspacesIn, 1, 1.21, almostLinear, fade"
+                "workspacesOut, 1, 1.94, almostLinear, fade"
+              ];
+            };
+
+            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+            dwindle = {
+              pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+              preserve_split = true; # You probably want this
+            };
+
+            # https://wiki.hyprland.org/Configuring/Variables/#misc
+            misc = {
+              force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
+              disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+            };
+
+            #############
+            ### INPUT ###
+            #############
+
+            # https://wiki.hyprland.org/Configuring/Variables/#input
+            input = {
+              kb_layout = "us";
+
+              follow_mouse = 1;
+
+              sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+
+              touchpad = {
+                scroll_factor = 0.8;
+                natural_scroll = true;
+              };
+            };
+
+            # https://wiki.hyprland.org/Configuring/Variables/#gestures
+            gesture = [
+              # Swipe with 3 fingers horizontally to scroll to the next workspace
+              "3, horizontal, workspace"
+              # Swip up with 3 fingers while holding `SUPER` to make a window fullscreen.
+              "3, up, mod: SUPER, scale: 1.5, fullscreen"
+            ];
+
+            ################
+            ### KEYBINDS ###
+            ################
             "$mainMod" = "SUPER"; # `Windows` key is the main modifier.
             bind = [
               "$mainMod, T, exec, $terminal"
@@ -103,6 +236,24 @@
               ", XF86AudioPause, exec, playerctl play-pause"
               ", XF86AudioPlay, exec, playerctl play-pause"
               ", XF86AudioPrev, exec, playerctl previous"
+            ];
+
+            ##############################
+            ### WINDOWS AND WORKSPACES ###
+            ##############################
+
+            # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+            # See https://wiki.hyprland.org/Configuring/Workspace-Rules/ for workspace rules
+
+            # Example windowrule
+            # windowrule = float,class:^(kitty)$,title:^(kitty)$
+
+            windowrule = [
+              # Ignore maximize requests from apps. You'll probably like this.
+              "suppressevent maximize, class:.*"
+
+              # Fix some dragging issues with XWayland
+              "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
             ];
           };
         };

2f8174ea1a37eb3a38e9a27b85374f31f4ec9148
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-24 08:11:16 -0500

    fix: Zoxide error popup

--- modules/core/programs/shell/zoxide.nix
+++ modules/core/programs/shell/zoxide.nix
@@ -1,17 +1,23 @@
 {
-  flake.modules.nixos.core = {
-    # Fast cd command that learns your habits
-    programs.zoxide = {
-      enable = true;
-      enableBashIntegration = true;
-    };
+  flake.modules.nixos.core =
+    { lib, pkgs, ... }:
+    {
+      # Fast cd command that learns your habits
+      programs.zoxide.enable = true;
 
-    environment.sessionVariables = {
-      _ZO_ECHO = 1;
-    };
+      # Disable automatic integration so we can place the eval command at the
+      # end ourselves.
+      programs.zoxide.enableBashIntegration = false;
+      programs.bash.interactiveShellInit = lib.mkOrder 2000 ''
+        eval "$(${lib.getExe pkgs.zoxide} init bash)"
+      '';
 
-    environment.shellAliases = {
-      cd = "z";
+      environment.sessionVariables = {
+        _ZO_ECHO = 1;
+      };
+
+      environment.shellAliases = {
+        cd = "z";
+      };
     };
-  };
 }

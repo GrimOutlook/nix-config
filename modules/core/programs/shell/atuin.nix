9f86e996cefb81392141d6e2fb41ec8ffd1ec6b1
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-25 09:00:10 -0500

    feat: Make `atuin` history session bound

--- modules/core/programs/shell/atuin.nix
+++ modules/core/programs/shell/atuin.nix
@@ -12,5 +12,20 @@
       programs.bash.interactiveShellInit = lib.mkOrder 1900 ''
         eval "$(atuin init bash)"
       '';
+
+      environment.etc."atuin/config.toml".text = ''
+        auto_sync = false
+        update_check = false
+
+        filter_mode = "host"
+        # Show session history first, and then global history before the session
+        # was started
+        filter_mode_shell_up_key_binding = "session-preload"
+
+        [stats]
+        common_subcommands = ["cargo", "git", "nix", "cd"]
+      '';
+
+      environment.variables.ATUIN_CONFIG_DIR = "/etc/atuin";
     };
 }

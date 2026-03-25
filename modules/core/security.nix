071a5c10fdd84567a9a9b6562eefb6242050aca9
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-24 20:21:18 -0500

    feat: Add `clamav` to for scans

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,17 +1,28 @@
 {
-  flake.modules.nixos.security = {
-    security = {
-      sudo-rs = {
-        enable = true;
-        extraRules = [
-          # Allow execution of any command by all users in group sudo,
-          # requiring a password.
-          {
-            groups = [ "sudo" ];
-            commands = [ "ALL" ];
-          }
-        ];
+  flake.modules.nixos.security =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        clamav
+      ];
+      services.clamav = {
+        daemon.enable = true;
+        scanner.enable = true;
+        updater.enable = true;
+      };
+
+      security = {
+        sudo-rs = {
+          enable = true;
+          extraRules = [
+            # Allow execution of any command by all users in group sudo,
+            # requiring a password.
+            {
+              groups = [ "sudo" ];
+              commands = [ "ALL" ];
+            }
+          ];
+        };
       };
     };
-  };
 }

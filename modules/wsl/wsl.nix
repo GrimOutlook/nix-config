d9e32ea2fb048e32da4ac0b04a1c314b2890d483
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-27 07:22:28 -0500

    feat: Add powershell.exe link to WSL

--- modules/wsl/wsl.nix
+++ modules/wsl/wsl.nix
@@ -1,19 +1,27 @@
 { config, inputs, ... }:
 {
-  flake.modules.nixos.wsl = {
-    imports = [
-      inputs.nixos-wsl.nixosModules.default
-    ];
-    wsl = {
-      enable = true;
-      defaultUser = config.meta.owner.username;
-      # NOTE: Including the path slows down commands and bash-completion
-      # significantly
-      interop.includePath = false;
-      wslConf.interop.appendWindowsPath = false;
-    };
+  flake.modules.nixos.wsl =
+    { pkgs, ... }:
+    {
+      imports = [
+        inputs.nixos-wsl.nixosModules.default
+      ];
+      wsl = {
+        enable = true;
+        defaultUser = config.meta.owner.username;
+        # NOTE: Including the path slows down commands and bash-completion
+        # significantly. We include some paths manually where desired.
+        interop.includePath = false;
+        wslConf.interop.appendWindowsPath = false;
+      };
 
-    # nftables seems to fail to start in WSL
-    networking.nftables.enable = false;
-  };
+      environment.systemPackages = with pkgs; [
+        # This is basically just an alias but I want it available to
+        # non-session process.
+        (writeShellScriptBin "powershell.exe" "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe $@")
+      ];
+
+      # nftables seems to fail to start in WSL
+      networking.nftables.enable = false;
+    };
 }

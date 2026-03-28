7000290c06bca8ff6ddbc010e6fcc3ad2b00f9f4
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-28 18:40:44 -0500

    fix: Add cache for all hosts, not just dev

--- capabilities/core/nix.nix
+++ capabilities/core/nix.nix
@@ -1,3 +1,17 @@
+let
+  substituters = [
+    {
+      url = "https://cache.nixos.org";
+      publicKey = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+      priority = 1;
+    }
+    {
+      url = "https://nix-community.cachix.org";
+      publicKey = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      priority = 2;
+    }
+  ];
+in
 {
   flake.modules = {
     nixos.nix = {
@@ -69,6 +83,10 @@
           ];
 
           builders-use-substitutes = true;
+
+          trusted-public-keys = builtins.catAttrs "publicKey" substituters;
+
+          substituters = builtins.map (def: "${def.url}?priority=${toString def.priority}") substituters;
 
           # If set to true, Nix will fall back to building from source if a binary substitute
           # fails. This is equivalent to the –fallback flag. The default is false.
--- capabilities/dev/default.nix
+++ capabilities/dev/default.nix
@@ -2,10 +2,6 @@
 {
   flake.modules = {
     nixos.dev = {
-      imports = with config.flake.modules.nixos; [
-        substituters
-      ];
-
       # Enable nix-ld for easier uv use
       programs.nix-ld.enable = true;
     };

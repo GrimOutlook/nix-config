eb196134c8f625a30ace7e45307d7a8e1dfe8ced
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-28 19:32:34 -0500

    fix: Bad arch option

--- host-types/pi/imports.nix
+++ host-types/pi/imports.nix
@@ -3,18 +3,19 @@
   flake.modules = {
     nixos.pi =
       { lib, pkgs, ... }:
+      with lib;
       {
         imports = [
           "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
         ];
-        nixpkgs.hostPlatform = "aarch64-linux";
+        nixpkgs.hostPlatform = mkDefault "aarch64-linux";
         nix.settings = {
           substituters = [ "https://raspberry-pi-nix.cachix.org" ];
           trusted-public-keys = [
             "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0KTFjanUjs096+7CwhvL32Ggca9M4w="
           ];
         };
-        system = "aarch64-linux";
+        system.autoUpgrade.enable = mkDefault true;
       };
     home.pi.imports = with config.flake.modules.home; [
     ];

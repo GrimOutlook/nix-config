72ec70f6b670732fbad8f3d788f5ab0cfef8c7e0
Dominic Grimaldi <dev@grimoutlook.dev>
2026-03-30 09:28:04 -0500

    fix: Move git hooks location

--- capabilities/dev/git/hooks.nix
+++ capabilities/dev/git/hooks.nix
@@ -1,13 +1,16 @@
 {
   flake.modules.homeManager.dev =
     { pkgs, ... }:
+    let
+      hooks_path = ".config/git/hooks";
+    in
     {
-      programs.git.settings.core.hooksPath = "~/.git-hooks";
-      home.file.".git-hooks/pre-commit" = {
+      programs.git.settings.core.hooksPath = "~/${hooks_path}";
+      home.file."${hooks_path}/pre-commit" = {
         executable = true;
         text = ''
           #!/bin/sh
-          for hook in ~/git-hooks/pre-commit.d/*; do
+          for hook in ~/${hooks_path}/pre-commit.d/*; do
             if [ -x "$hook" ]; then
               "$hook" "$@" || exit 1
             fi

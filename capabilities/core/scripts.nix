75e6948392a68edc981dcca5ffc07909c7a82850
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-06 18:13:04 -0500

    refactor: Remove duplicate code for script import

--- capabilities/core/scripts.nix
+++ capabilities/core/scripts.nix
@@ -1,17 +1,18 @@
 {
   flake.modules.nixos.core =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
+    let
+      scriptDir = ./scripts;
+      scriptNames = builtins.attrNames (
+        lib.filterAttrs (name: type: type == "regular") (builtins.readDir scriptDir)
+      );
+    in
     {
       environment.shellAliases = {
         macs = ''grep -Po "([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})"'';
         ips = ''grep -Po "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"'';
       };
-      environment.systemPackages = with pkgs; [
-        (writeShellScriptBin "macvendor" (builtins.readFile ./scripts/macvendor))
-        (writeShellScriptBin "certscrape" (builtins.readFile ./scripts/certscrape))
-        (writeShellScriptBin "materialize" (builtins.readFile ./scripts/materialize))
-        (writeShellScriptBin "cs" (builtins.readFile ./scripts/cs))
-        (writeShellScriptBin "acronym" (builtins.readFile ./scripts/acronym))
-      ];
+      environment.systemPackages =
+        map (name: pkgs.writeShellScriptBin name (builtins.readFile (scriptDir + "/${name}"))) scriptNames;
     };
 }

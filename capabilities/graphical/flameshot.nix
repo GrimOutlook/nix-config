02589b928787d7b5f900c7f1ae31e128a8be1d00
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-07 14:02:02 -0500

    feat: Add screenshot support to graphical hosts

---
+++ capabilities/graphical/flameshot.nix
@@ -0,0 +1,52 @@
+{ config, ... }:
+{
+  flake.modules.homeManager.graphical =
+    { lib, pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        grim
+      ];
+      services.flameshot = {
+        enable = true;
+        settings = {
+          General = {
+
+            # More settings may be found on the Flameshot Github
+
+            # Save Path
+            savePath = "/home/${config.meta.owner.username}/Pictures/Screenshots";
+            # Tray
+            disabledTrayIcon = true;
+            # Greeting message
+            showStartupLaunchMessage = false;
+            # Default file extension for screenshots (.png by default)
+            saveAsFileExtension = ".png";
+            # Desktop notifications
+            showDesktopNotification = true;
+            # Notification for cancelled screenshot
+            showAbortNotification = false;
+            # Whether to show the info panel in the center in GUI mode
+            showHelp = true;
+            # Whether to show the left side button in GUI mode
+            showSidePanelButton = true;
+
+            # Color Customization
+            uiColor = "#740096";
+            contrastUiColor = "#270032";
+            drawColor = "#ff0000";
+
+            # For Wayland (Install Grim seperately)
+            useGrimAdapter = true;
+            # Stops warnings for using Grim
+            disabledGrimWarning = true;
+          };
+        };
+      };
+      wayland.windowManager.hyprland.bind = lib.mkAfter [
+        # Takes a manual screenshot that shows options after selection.
+        "$mainMod, PRINT, exec, flameshot gui"
+        # Manual screenshot GUI that saves to clipboard and closes on selection.
+        "PRINT, exec, flameshot gui --clipboard --accept-on-select"
+      ];
+    };
+}

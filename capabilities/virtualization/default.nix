659c74e188b8a329c9f73a6ab9871fc4a2d07f83
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-11 09:48:24 -0500

    wip!

--- capabilities/virtualization/default.nix
+++ capabilities/virtualization/default.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  nix-config-inputs,
+  inputs,
   pkgs,
   ...
 }:
@@ -9,12 +9,13 @@ let
   cfg = config.host.virtualization;
 in
 {
+  imports = [
+    inputs.nix-config.inputs.microvm.nixosModules.host
+  ];
+
   options.host.virtualization.enable = lib.mkEnableOption "Enable virtualization configurations";
 
   config = lib.mkIf cfg.enable {
-    imports = [
-      nix-config-inputs.microvm.nixosModules.host
-    ];
 
     environment.systemPackages = with pkgs; [
       podman

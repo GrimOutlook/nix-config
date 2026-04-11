15809bca7b6f428dd2b55b3d483bdefcb7140beb
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-10 21:19:56 -0500

    Modularize network-diag module

---
+++ capabilities/network-diag/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.host.network-diag;
+in
+{
+  options.host.network-diag.enable = lib.mkEnableOption "Enable network diag configurations";
+
+  config = lib.mkIf cfg.enable {
+    host.home-manater.home.packages = with pkgs; [
+      # API for GeoIP/Geolocation databases
+      # https://www.maxmind.com/
+      geoip
+
+      # Network scanner with features like WiFi scanning, packetdump and more
+      # https://github.com/Chleba/netscanner
+      netscanner
+
+      # Free and open source utility for network discovery and security auditing
+      # http://www.nmap.org/
+      nmap
+
+      # Network packet analyzer
+      # https://github.com/jpr5/ngrep/
+      ngrep # Network packet analyzer
+
+      # Port listener and reverse shell
+      # https://github.com/robiot/rustcat
+      rustcat
+
+      # TODO: Not currently in nixpkgs repo
+      # # A cross-platform network monitoring terminal UI tool built with Rust.
+      # # https://github.com/domcyrus/rustnet
+      # rustnet
+
+      # The Modern Port Scanner.
+      # https://github.com/bee-san/RustScan
+      # NOTE: Still need `nmap` for deep inspection since this tool only does
+      # port scans.
+      rustscan
+    ];
+
+    home.shellAliases = {
+      nc = "rcat";
+      netcat = "rcat";
+      scan = "rustscan";
+    };
+  };
+}

373a3704275ca5b5951c8c5540b8169be66379fb
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-13 06:46:55 -0500

    feat: Re-add extra certificates module

---
+++ capabilities/misc/certificates.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  certificates = config.services.import-certificates;
+in
+{
+  options.services.import-certificates = lib.mkOption {
+    type = lib.types.listOf lib.types.path;
+    default = [ ];
+    description = "Extra certificates to trust";
+  };
+
+  config = lib.mkIf (certificates != [ ]) {
+    environment.systemPackages = [
+      pkgs.cacert # Bundle of X.509 certificates of public Certificate Authorities (CA)
+    ];
+    security.pki.certificateFiles = [
+      "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    ]
+    ++ map (file: pkgs.runCommand "copy-certificate-${file}" { } "cp ${file} $out") certificates;
+  };
+}

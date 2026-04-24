ec84a8273aca5583524b3790ad370ff0b169e5c8
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-23 19:02:56 -0500

    fix: Disable microvm for the moment

--- host-types/vm/default.nix
+++ host-types/vm/default.nix
@@ -8,18 +8,18 @@ let
   cfg = config.host.type.virtual-machine;
 in
 {
-  imports = [
-    inputs.nix-config.inputs.microvm.nixosModules.microvm
-  ];
+  # imports = [
+  #   inputs.nix-config.inputs.microvm.nixosModules.microvm
+  # ];
 
   options.host.type.virtual-machine.enable =
     lib.mkEnableOption "Enable virtual-machine configurations";
 
   config = {
-    microvm = {
-      guest.enable = (cfg.enable);
-      hypervisor = lib.mkDefault "cloud-hypervisor";
-    };
+    # microvm = {
+    #   guest.enable = (cfg.enable);
+    #   hypervisor = lib.mkDefault "cloud-hypervisor";
+    # };
     # host.metrics = true;
     # notifications.ssh-server = true;
   };

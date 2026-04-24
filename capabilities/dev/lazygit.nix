2d59c6e410aacaec2c749f3cd1a530a3f6050515
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-24 10:30:31 -0500

    feat: re-add lazygit back to dev

---
+++ capabilities/dev/lazygit.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.host.dev.lazygit;
+in
+{
+  options.host.dev.lazygit.enable = lib.mkEnableOption "Enable lazygit configuration";
+
+  config.host.home-manager.config = lib.mkIf cfg.enable {
+    programs.lazygit = {
+      enable = true;
+      enableBasheIntegration = true;
+      settings = {
+        gui.theme = {
+          activeBorderColor = [
+            "blue"
+            "bold"
+          ];
+          selectedLineBgColor = [ "white" ];
+        };
+
+        git = {
+          # Improves performance
+          # https://github.com/jesseduffield/lazygit/issues/2875#issuecomment-1665376437
+          log.order = "default";
+
+          fetchAll = false;
+        };
+      };
+    };
+  };
+}

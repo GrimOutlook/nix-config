82ef3f2774fa9dca17ad60a1fc2b8a42a26ee0b5
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-23 19:04:08 -0500

    feat: Disable raspberry pi configurations for the moment

--- host-types/pi/default.nix
+++ host-types/pi/default.nix
@@ -9,13 +9,13 @@ in
 {
   options.host.type.raspberry-pi.enable = lib.mkEnableOption "Enable raspberry-pi configurations";
 
-  config = lib.mkIf cfg.enable {
-    nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
-    nix.settings = {
-      substituters = [ "https://raspberry-pi-nix.cachix.org" ];
-      trusted-public-keys = [
-        "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0KTFjanUjs096+7CwhvL32Ggca9M4w="
-      ];
-    };
-  };
+  # config = lib.mkIf cfg.enable {
+  #   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  #   nix.settings = {
+  #     substituters = [ "https://raspberry-pi-nix.cachix.org" ];
+  #     trusted-public-keys = [
+  #       "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0KTFjanUjs096+7CwhvL32Ggca9M4w="
+  #     ];
+  #   };
+  # };
 }

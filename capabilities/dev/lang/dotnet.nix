f286ccea31fb6fd809df2961973eb7299f01305c
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-28 14:26:34 -0500

    fix: Unnest dotnet lang definition

--- capabilities/dev/lang/dotnet.nix
+++ capabilities/dev/lang/dotnet.nix
@@ -10,18 +10,16 @@ in
 {
   options.host.dev.lang.dotnet.enable = lib.mkEnableOption "Enable .NET/C# language support";
 
-  config = lib.mkIf cfg.enable {
-    host.home-manager.config = {
-      home.packages = with pkgs; [
-        csharpier
-      ];
+  config.host.home-manager.config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      csharpier
+    ];
 
-      programs.nixvim = {
-        lsp.servers.csharp_ls.enable = true;
-        plugins = {
-          easy-dotnet.enable = true;
-          conform-nvim.settings.formatters_by_ft.cs = [ "csharpier" ];
-        };
+    programs.nixvim = {
+      lsp.servers.csharp_ls.enable = true;
+      plugins = {
+        easy-dotnet.enable = true;
+        conform-nvim.settings.formatters_by_ft.cs = [ "csharpier" ];
       };
     };
   };

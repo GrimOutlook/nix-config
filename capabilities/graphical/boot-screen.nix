288b931fcec43858ce20eb74338a3966c27225cb
Dominic Grimaldi <dev@grimoutlook.dev>
2026-05-01 10:39:27 -0500

    fix(boot-screen): Add optimizations to improve framerate

--- capabilities/graphical/boot-screen.nix
+++ capabilities/graphical/boot-screen.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.host.boot-screen;
+  inherit (lib) mkDefault;
 in
 {
   options.host.boot-screen.enable = lib.mkEnableOption "Enable boot screen (plymouth)";
@@ -24,11 +25,21 @@ in
 
     # Enable "Silent boot"
     consoleLogLevel = 3;
-    initrd.verbose = false;
+    initrd = {
+      # WARN: Very experimental at the moment but makes plymouth start much earlier.
+      systemd.enable = mkDefault true;
+      verbose = mkDefault false;
+    };
     kernelParams = [
       "quiet"
-      "udev.log_level=3"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
       "systemd.show_status=auto"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "udev.log_level=3"
     ];
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key

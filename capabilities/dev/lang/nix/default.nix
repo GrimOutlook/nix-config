72eb72a132f2c5efc6715e73759c3e78cf4ff9a1
Dominic Grimaldi <dev@grimoutlook.dev>
2026-05-02 04:47:08 -0500

    feat(nix): Add `nix-prefetch-github` to nix dev config

--- capabilities/dev/lang/nix/default.nix
+++ capabilities/dev/lang/nix/default.nix
@@ -10,53 +10,46 @@ in
 {
   options.host.dev.lang.nix.enable = lib.mkEnableOption "Enable Nix language support";
 
-  config = lib.mkIf cfg.enable {
-    host.home-manager.config = {
-      home = {
-        packages = with pkgs; [
-          alejandra
-          nix-tree
-          nix-output-monitor
-          statix
-          patchelf
+  config.host.home-manager.config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      alejandra
+      nix-tree
+      nix-output-monitor
+      nix-prefetch-github
+      statix
+      patchelf
 
-          # nixpkgs
-          hydra-check
-          nix-update
-          nixpkgs-review
-          nixfmt
-          nixfmt-tree
-          luarocks-packages-updater
-          vimPluginsUpdater
-        ];
+      # nixpkgs
+      hydra-check
+      nix-update
+      nixpkgs-review
+      nixfmt
+      nixfmt-tree
+      luarocks-packages-updater
+      vimPluginsUpdater
+    ];
 
-        sessionVariables.NIXPKGS_ALLOW_UNFREE = 1;
+    programs = {
+      nix-init = {
+        enable = true;
+        settings.maintainers = [ "GrimOutlook" ];
+
       };
 
-      programs = {
-        nix-init = {
+      nixvim = {
+        lsp.servers.nil_ls = {
           enable = true;
-          settings = {
-            maintainers = [ "GrimOutlook" ];
-          };
+          config.settings.formatting.command = [
+            (lib.getExe pkgs.nixfmt)
+          ];
         };
 
-        nixvim = {
-          lsp.servers.nil_ls = {
-            enable = true;
-            config.settings.formatting.command = [
-              (lib.getExe pkgs.nixfmt)
-            ];
-          };
-
-          files."after/ftplugin/nix.lua" = {
-            # Set indentation to 2 spaces
-            localOpts = {
-              tabstop = 2;
-              shiftwidth = 2;
-            };
-          };
+        # Set indentation to 2 spaces
+        files."after/ftplugin/nix.lua".localOpts = {
+          tabstop = 2;
+          shiftwidth = 2;
         };
+
       };
     };
   };

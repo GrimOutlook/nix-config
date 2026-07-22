0f08d46454b4206bad5ac195eaadd45945361332
Dominic Grimaldi <dev@grimoutlook.dev>
2026-07-22 08:36:52 -0500

    fix(core/tide): use universal variables (-U) so context display persists across fish reloads

--- capabilities/core/programs/configs/tide.nix
+++ capabilities/core/programs/configs/tide.nix
@@ -11,9 +11,9 @@ let
     if not set -q tide_left_prompt_items
         tide configure --auto --style=Lean --prompt_colors="True color" --show_time=No --lean_prompt_height="One line" --prompt_connection_andor_frame_color="Light" --prompt_spacing=Compact --icons="Few icons" --transient=No >/dev/null 2>&1
     end
-    set -g tide_context_always_display true
+    set -U tide_context_always_display true
     if not contains context $tide_left_prompt_items
-        set -g tide_left_prompt_items context $tide_left_prompt_items
+        set -U tide_left_prompt_items context $tide_left_prompt_items
     end
   '';
 in

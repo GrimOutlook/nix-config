5719dc4c43aa789925fd6084df127cdfbbbe2c2e
Dominic Grimaldi <dev@grimoutlook.dev>
2026-09-11 18:15:52 -0500

    fix(devshell): use passwordless deployment elevation

--- flake/host-lib.nix
+++ flake/host-lib.nix
@@ -26,7 +26,7 @@ let
           command = ''
             target="${hostname}"
             run_deploy() {
-              nh os switch . -H "$target" --keep-going --show-activation-logs "$@"
+              nh os switch . -H "$target" --keep-going --show-activation-logs --elevation-strategy=passwordless "$@"
             }
             if [ "$(uname -n)" = "$target" ]; then
               echo "=> Deploying to local host: $target"
